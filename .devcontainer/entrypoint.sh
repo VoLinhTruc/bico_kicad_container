ae14f1ae90b9e7a4123b1103cdae8ed9e880a08d
Vo Linh Truc (MS/EAE32-XC) <Truc.VoLinh@vn.bosch.com>
2025-12-03 23:07:02 +0700

    comment out the dark theme setup, get back to KiCAD default theme. Reason: easier to color the schematic, in dark theme even the wrire colore set to green or red, what I see is dark red

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -3,20 +3,21 @@
 # enable permission for user to/workspaces
 sudo chown -R developer:developer /workspaces
 
+# create kicad config dir
+mkdir -p /home/developer/.config/kicad/${KICAD_VERSION}
+
+# update and copy config to KiCAD
+python /workspaces/bico_kicad_container/kicad/config/kicad_common.py
+sudo cp /workspaces/bico_kicad_container/kicad/config/kicad_common.json /home/developer/.config/kicad/${KICAD_VERSION}/kicad_common.json
+
+# Theme setup - begin ------------------------------------------------------------
 # download dark theme color
 curl -L https://raw.githubusercontent.com/pointhi/kicad-color-schemes/2021-12-05/wdark/wdark.json \
 -o /workspaces/bico_kicad_container/kicad/config/color_theme/wdark.json
 mkdir -p ~/.local/share/kicad/9.0/3rdparty/colors/com_github_pointhi_kicad-color-schemes_wdark
 cp /workspaces/bico_kicad_container/kicad/config/color_theme/wdark.json ~/.local/share/kicad/9.0/3rdparty/colors/com_github_pointhi_kicad-color-schemes_wdark/wdark.json
 
-# create kicad config dir
-mkdir -p /home/developer/.config/kicad/${KICAD_VERSION}
-
-# update and copy color theme to KiCAD
-python /workspaces/bico_kicad_container/kicad/config/kicad_common.py
-sudo cp /workspaces/bico_kicad_container/kicad/config/eeschema.json /home/developer/.config/kicad/${KICAD_VERSION}/eeschema.json
-
-# update and copy config to KiCAD
-python /workspaces/bico_kicad_container/kicad/config/kicad_common.py
-sudo cp /workspaces/bico_kicad_container/kicad/config/kicad_common.json /home/developer/.config/kicad/${KICAD_VERSION}/kicad_common.json
-
+# # update and copy color theme to KiCAD
+# python /workspaces/bico_kicad_container/kicad/config/kicad_common.py
+# sudo cp /workspaces/bico_kicad_container/kicad/config/eeschema.json /home/developer/.config/kicad/${KICAD_VERSION}/eeschema.json
+# Theme setup - end ------------------------------------------------------------
